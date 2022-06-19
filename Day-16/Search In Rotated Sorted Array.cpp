9d4b82ff591a67b61ab90ffde71d5a9c9a79d5fe
Diya Soor <89120515+diyasoor@users.noreply.github.com>
2022-06-19 11:30:25 +0530

    Add files via upload

---
+++ Day-16/Search In Rotated Sorted Array.cpp
@@ -0,0 +1,17 @@
+int search(int* arr, int n, int key) {
+    // Write your code here.
+    int lo = 0,hi = n - 1;
+    while(lo <= hi) {
+        int mid = (lo + hi)/2;
+        if(arr[mid]==key) return mid;
+        if(arr[lo] <= arr[mid]) {
+            if(key>=arr[lo] && key<=arr[mid]) hi = mid - 1;
+            else lo = mid + 1;
+        }
+        else {
+            if(key>=arr[mid] && key<=arr[hi]) lo = mid + 1;
+            else hi = mid - 1;
+        }
+    }
+    return -1;
+}
