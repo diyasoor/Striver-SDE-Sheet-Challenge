0dd6ece25ddc0b446210bb95eab3ab83f240345b
Diya Soor <89120515+diyasoor@users.noreply.github.com>
2022-06-23 10:19:14 +0530

    Add files via upload

---
+++ Day-20/Check Permutation.cpp
@@ -0,0 +1,13 @@
+bool areAnagram(string &str1, string &str2){
+    // Write your code here.
+    if(str1.length()!=str2.length()) return false;
+    int freq[26] = {0};
+    for(int i=0;i<str1.length();i++) {
+        freq[str1[i] - 'a']++;
+        freq[str2[i] - 'a']--;
+    }
+    for(int i=0;i<26;i++) {
+        if(freq[i]) return false;
+    }
+    return true;
+}
