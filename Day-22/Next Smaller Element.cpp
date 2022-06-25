84c4bfab753ade2e2eeb1b6f54353b1509a481de
Diya Soor <89120515+diyasoor@users.noreply.github.com>
2022-06-25 23:40:50 +0530

    Add files via upload

---
+++ Day-22/Next Smaller Element.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+vector<int> nextSmallerElement(vector<int> &arr, int n)
+{
+    // Write your code here.
+    vector<int> v;
+    stack<int> s;
+    for(int i=arr.size()-1;i>=0;i--) {
+        if(s.empty()) v.push_back(-1);
+        else if(s.top() < arr[i]) v.push_back(s.top());
+        else {
+            // pop the stack until u find the greatest element
+            while(!s.empty() && s.top()>=arr[i]) s.pop();
+            if(s.empty()) v.push_back(-1);
+            else if(s.top() < arr[i]) v.push_back(s.top());
+        }
+        s.push(arr[i]);
+    }
+    reverse(begin(v),end(v));
+    return v;
+}
