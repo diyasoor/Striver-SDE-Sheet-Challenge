ca2b8e72819410e2787679dc37973f916848ce66
Diya Soor <89120515+diyasoor@users.noreply.github.com>
2022-06-28 21:22:07 +0530

    Add files via upload

---
+++ Day-25/Kth smallest node in BST.cpp
@@ -0,0 +1,33 @@
+/************************************************************
+
+    Following is the TreeNode class structure
+
+    template <typename T>
+    class TreeNode {
+       public:
+        T data;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+
+        TreeNode(T data) {
+            this->data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+************************************************************/
+void inorder(TreeNode<int>* root, int& k, int &ans) {
+    if(!root) return;
+    inorder(root->left,k,ans);
+    k--;
+    if(k==0) ans = root->data;
+    inorder(root->right,k,ans);
+}
+int kthSmallest(TreeNode<int> *root, int k)
+{
+	//	Write the code here.
+    int ans = -1;
+    inorder(root,k,ans);
+    return ans;
+}
