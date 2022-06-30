23fc6dc5fd209915793577be34ece59e4f6cb283
Diya Soor <89120515+diyasoor@users.noreply.github.com>
2022-06-30 19:52:18 +0530

    Add files via upload

---
+++ Day-27/Search in BST.cpp
@@ -0,0 +1,27 @@
+/*
+    Following is the Binary Tree node structure:
+
+    class BinaryTreeNode {
+    public : 
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+
+        BinaryTreeNode(T data) {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+*/
+
+bool searchInBST(BinaryTreeNode<int> *root, int x) {
+    // Write your code here.
+    if(!root) return false;
+    while(root) {
+        if(root->data > x) root = root->left;
+        else if(root->data < x) root = root->right;
+        else return true;
+    }
+    return false;
+}
