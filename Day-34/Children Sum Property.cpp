51cc4c42d14a4f0ec07f62c35047b527d8de4a6b
Diya Soor <89120515+diyasoor@users.noreply.github.com>
2022-07-07 14:01:40 +0530

    Add files via upload

---
+++ Day-34/Children Sum Property.cpp
@@ -0,0 +1,40 @@
+/*************************************************************
+
+    Following is the Binary Tree node structure
+
+    class BinaryTreeNode
+    {
+    public :
+        T data;
+        BinaryTreeNode < T > *left;
+        BinaryTreeNode < T > *right;
+
+        BinaryTreeNode(T data) {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+*************************************************************/
+void changeTree(BinaryTreeNode < int > * root) {
+    // Write your code here.
+    if(!root) return;
+    int child=0;
+    if(root->left) child+=root->left->data;
+    if(root->right) child+=root->right->data;
+    
+    if(child < root->data) {
+        if(root->left) root->left->data = root->data;
+        if(root->right) root->right->data = root->data;
+    }
+    else root->data = child;
+    
+    changeTree(root->left);
+    changeTree(root->right);
+    
+    int total=0;
+    if(root->left) total+=root->left->data;
+    if(root->right) total+=root->right->data;
+    if(root->left or root->right) root->data = total;
+}  
